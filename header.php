5afbf627a8e6c4814b5af6827b22aeb2a1d7e29b
ngoytrylyhuor <lyhourkhe123@gmail.com>
2024-04-28 17:19:47 +0700

    qw

--- header.php
+++ header.php
@@ -11,8 +11,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Hanuman:wght@100;300;400;700;900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/swiper.css?v=1.0" rel="stylesheet">
-	<link href="css/styles.css" rel="stylesheet">
+    <link href="css/swiper.css" rel="stylesheet">
+	<link href="css/styles.css?v=1.0" rel="stylesheet">
 	
 	<!-- Favicon  -->
     <link rel="icon" href="images/logo.png">
