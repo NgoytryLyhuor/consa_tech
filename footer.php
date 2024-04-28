c0c57a76be7501dbf699e1ffc251ca35a8d37253
ngoytrylyhuor <lyhourkhe123@gmail.com>
2024-04-28 17:22:35 +0700

    a11111111

--- footer.php
+++ footer.php
@@ -107,7 +107,7 @@
     <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
     <script src="js/purecounter.min.js"></script> <!-- Purecounter counter for statistics numbers -->
     <script src="js/isotope.pkgd.min.js"></script> <!-- Isotope for filter -->
-    <script src="js/scripts.js?v=1.0"></script> <!-- Custom scripts -->
+    <script src="js/scripts.js"></script> <!-- Custom scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
 </body>
